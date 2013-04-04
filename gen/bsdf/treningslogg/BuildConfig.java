3be444047940918064cd254e9f99dc14a4c09db9
SindreN <sind_nj@hotmail.com>
2013-04-05 00:02:56 +0200

    Opprettet prosjektet.

---
+++ gen/bsdf/treningslogg/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package bsdf.treningslogg;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ gen/bsfd/treningslogg/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package bsfd.treningslogg;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
